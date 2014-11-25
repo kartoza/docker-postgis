00644eb5192a053bf76f1a657aa6a99ace8d0289
Tim Sutton <tim@linfiniti.com>
2014-11-25 11:18:16 +0200

    Possible fix for #3 - permission denied for ssl - part 2

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ CONF="/etc/postgresql/9.3/main/postgresql.conf"
 # (@andrewgodwin says it's something AUFS related)  - taken from https://github.com/orchardup/docker-postgresql/blob/master/Dockerfile
 cp -r /etc/ssl /tmp/ssl-copy/
 chmod -R 0700 /etc/ssl
-chown -R postgres /etc/ssl
+chown -R postgres /tmp/ssl-copy
 rm -r /etc/ssl
 mv /tmp/ssl-copy /etc/ssl
 
