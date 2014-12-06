5116a31255ba9ff62c3892e4b4691bc043fde553
Tim Sutton <tim@linfiniti.com>
2014-12-06 11:33:36 +0700

    Move permissions for ssl to be run as root

--- perms_wrapper.sh
+++ perms_wrapper.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# /etc/ssl/private can't be accessed from within container for some reason
+# (@andrewgodwin says it's something AUFS related)  - taken from https://github.com/orchardup/docker-postgresql/blob/master/Dockerfile
+cp -r /etc/ssl /tmp/ssl-copy/
+chmod -R 0700 /etc/ssl
+chown -R postgres /tmp/ssl-copy
+rm -r /etc/ssl
+mv /tmp/ssl-copy /etc/ssl
+
 # needs to be done as root:
 chown -R postgres:postgres /var/lib/postgresql
 
