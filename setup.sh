59526fd3a19912c993d8e09e9224f12d21597bd7
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2015-05-18 18:59:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,18 +3,20 @@
 # These tasks are run as root
 CONF="/etc/postgresql/9.4/main/postgresql.conf"
 
+# /etc/ssl/private can't be accessed from within container for some reason
+# (@andrewgodwin says it's something AUFS related)  - taken from https://github.com/orchardup/docker-postgresql/blob/master/Dockerfile
+mkdir -p /etc/ssl/private-copy
+mv /etc/ssl/private/* /etc/ssl/private-copy/
+rm -r /etc/ssl/private
+mv /etc/ssl/private-copy /etc/ssl/private
+chmod -R 0700 /etc/ssl/private
+chown -R postgres /etc/ssl/private
 
 # Restrict subnet to docker private network
 echo "host    all             all             0.0.0.0/0                   md5" >> /etc/postgresql/9.4/main/pg_hba.conf
-#echo "host    replication     postgres        0.0.0.0/0                   trust" >> /etc/postgresql/9.4/main/pg_hba.conf
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> /etc/postgresql/9.4/main/postgresql.conf
 echo "port = 5432" >> /etc/postgresql/9.4/main/postgresql.conf
-#replication parameters
-#echo "wal_level = logical" >> /etc/postgresql/9.4/main/postgresql.conf
-#echo "max_wal_senders = 3" >> /etc/postgresql/9.4/main/postgresql.conf
-#echo "max_replication_slots = 3" >> /etc/postgresql/9.4/main/postgresql.conf
-#echo "hot_standby = on" >> /etc/postgresql/9.4/main/postgresql.conf
 
 # Enable ssl
 
@@ -24,4 +26,4 @@ echo "ssl = true" >> $CONF
 echo "ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'" >> $CONF 
 echo "ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'" >> $CONF 
 #echo "ssl_ca_file = ''                       # (change requires restart)" >> $CONF 
-#echo "ssl_crl_file = ''" >> $CONF 
+#echo "ssl_crl_file = ''" >> $CONF
