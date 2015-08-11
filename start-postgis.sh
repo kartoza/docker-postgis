cd39da18fe543e839cf3267693faf4ef631f6ee2
Tim Sutton <tim@kartoza.com>
2015-08-11 23:54:59 +0200

    Fix incorrect version in 9.4 branch

--- start-postgis.sh
+++ start-postgis.sh
@@ -2,11 +2,11 @@
 
 # This script will run as the postgres user due to the Dockerfile USER directive
 
-DATADIR="/var/lib/postgresql/9.3/main"
-CONF="/etc/postgresql/9.3/main/postgresql.conf"
-POSTGRES="/usr/lib/postgresql/9.3/bin/postgres"
-INITDB="/usr/lib/postgresql/9.3/bin/initdb"
-SQLDIR="/usr/share/postgresql/9.3/contrib/postgis-2.1/"
+DATADIR="/var/lib/postgresql/9.4/main"
+CONF="/etc/postgresql/9.4/main/postgresql.conf"
+POSTGRES="/usr/lib/postgresql/9.4/bin/postgres"
+INITDB="/usr/lib/postgresql/9.4/bin/initdb"
+SQLDIR="/usr/share/postgresql/9.4/contrib/postgis-2.1/"
 
 # /etc/ssl/private can't be accessed from within container for some reason
 # (@andrewgodwin says it's something AUFS related)  - taken from https://github.com/orchardup/docker-postgresql/blob/master/Dockerfile
@@ -17,8 +17,8 @@ rm -r /etc/ssl
 mv /tmp/ssl-copy /etc/ssl
 
 # Needed under debian, wasnt needed under ubuntu
-mkdir /var/run/postgresql/9.3-main.pg_stat_tmp
-chmod 0777 /var/run/postgresql/9.3-main.pg_stat_tmp
+mkdir /var/run/postgresql/9.4-main.pg_stat_tmp
+chmod 0777 /var/run/postgresql/9.4-main.pg_stat_tmp
 
 # test if DATADIR is existent
 if [ ! -d $DATADIR ]; then
@@ -123,7 +123,7 @@ fi
 # This should show up in docker logs afterwards
 su - postgres -c "psql -l"
 
-PID=`cat /var/run/postgresql/9.3-main.pid`
+PID=`cat /var/run/postgresql/9.4-main.pid`
 kill -9 ${PID}
 echo "Postgres initialisation process completed .... restarting in foreground"
 su - postgres -c "$POSTGRES -D $DATADIR -c config_file=$CONF"
