784d9ac0d519fae4e0702f2437dbe2344ce02fee
Tim Sutton <tim@kartoza.com>
2015-09-22 18:18:17 +0700

    Fix for #15

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ CONF="/etc/postgresql/9.3/main/postgresql.conf"
 
 # Restrict subnet to docker private network
 echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+# And allow access from DockerToolbox / Boottodocker on OSX
+echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.4/main/pg_hba.conf
+
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
 echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
