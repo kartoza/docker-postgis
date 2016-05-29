34d77e359c25e1765fdba37e24e02f04f066c862
Wisu Suntoyo <wisu@bigwisu.com>
2016-05-29 12:50:12 +0700

    use postgres 9.3

--- setup.sh
+++ setup.sh
@@ -2,20 +2,16 @@
 chmod 600 /etc/ssl/private/ssl-cert-snakeoil.key
 
 # These tasks are run as root
-CONF="/etc/postgresql/9.5/main/postgresql.conf"
+CONF="/etc/postgresql/9.3/main/postgresql.conf"
 
 # Restrict subnet to docker private network
-<<<<<<< HEAD
 echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 # And allow access from DockerToolbox / Boottodocker on OSX
-echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.4/main/pg_hba.conf
+echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 
-=======
-echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
->>>>>>> kartoza/develop
 # Listen on all ip addresses
-echo "listen_addresses = '*'" >> /etc/postgresql/9.5/main/postgresql.conf
-echo "port = 5432" >> /etc/postgresql/9.5/main/postgresql.conf
+echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
+echo "port = 5432" >> /etc/postgresql/9.3/main/postgresql.conf
 
 # Enable ssl
 
