cf95d4760a5b1b876c1e7e6c7f3c7c0d154142aa
Tim Sutton <tim@kartoza.com>
2016-09-06 12:06:47 +0200

    9.5 2.2 (#49)
    * Allow connections from 192.168 private network too
    
    * Updated references to pg and postgis versions in readme
    
    * Cleanups to 9.5 branch - remove old supervisor files
    
    * Fix for README

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ CONF="/etc/postgresql/9.5/main/postgresql.conf"
 
 # Restrict subnet to docker private network
 echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+# And allow access from DockerToolbox / Boottodocker on OSX
+echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.4/main/pg_hba.conf
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> /etc/postgresql/9.5/main/postgresql.conf
 echo "port = 5432" >> /etc/postgresql/9.5/main/postgresql.conf
@@ -14,8 +16,8 @@ echo "port = 5432" >> /etc/postgresql/9.5/main/postgresql.conf
 
 echo "ssl = true" >> $CONF
 #echo "ssl_ciphers = 'DEFAULT:!LOW:!EXP:!MD5:@STRENGTH' " >> $CONF
-#echo "ssl_renegotiation_limit = 512MB "  >> $CONF 
-echo "ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'" >> $CONF 
-echo "ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'" >> $CONF 
-#echo "ssl_ca_file = ''                       # (change requires restart)" >> $CONF 
-#echo "ssl_crl_file = ''" >> $CONF 
+#echo "ssl_renegotiation_limit = 512MB "  >> $CONF
+echo "ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'" >> $CONF
+echo "ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'" >> $CONF
+#echo "ssl_ca_file = ''                       # (change requires restart)" >> $CONF
+#echo "ssl_crl_file = ''" >> $CONF
