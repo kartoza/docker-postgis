5aad659c0c0c8b85a767f769a68695d5b84bae92
Wisu Suntoyo <wisu@bigwisu.com>
2017-03-27 11:52:05 +0700

    9.3 2.1 (#43)
    * Merge some tweaks from develop - fix container name reference in build and run script.
    * Fixed incorrect reference to pg 9.4

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # Add any additional setup tasks here
+chmod 600 /etc/ssl/private/ssl-cert-snakeoil.key
 
 # These tasks are run as root
 CONF="/etc/postgresql/9.3/main/postgresql.conf"
@@ -6,7 +7,7 @@ CONF="/etc/postgresql/9.3/main/postgresql.conf"
 # Restrict subnet to docker private network
 echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 # And allow access from DockerToolbox / Boottodocker on OSX
-echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.4/main/pg_hba.conf
+echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
