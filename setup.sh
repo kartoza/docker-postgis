1aa1b0de521d78049690900c0209ea52dc8e786a
Denis Rouzaud <denis.rouzaud@gmail.com>
2017-09-25 04:41:44 +0200

    allow connection when using docker compose (#65)
    see https://github.com/docker/compose/issues/4336
    and https://github.com/kartoza/docker-postgis/issues/40

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ CONF="/etc/postgresql/9.5/main/postgresql.conf"
 
 # Restrict subnet to docker private network
 echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+echo "host    all             all             172.18.0.0/16               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 # And allow access from DockerToolbox / Boottodocker on OSX
 echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 # Listen on all ip addresses
