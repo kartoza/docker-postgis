ca1a6157da3d15e55349c3da8b217f27fb58ecca
admire <admire@kartoza.com>
2019-04-28 14:16:37 +0200

    fix pid error on startup of replication

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,6 @@
 # This script will run as the postgres user due to the Dockerfile USER directive
 set -e
 
-#TODO Prepare lock files that prevent running the setup-conf,setup-pg_hba,setup-ssl.sh on each restart
 # Setup postgres CONF file
 
 source /setup-conf.sh
@@ -14,27 +13,6 @@ source /setup-ssl.sh
 # Setup pg_hba.conf
 
 source /setup-pg_hba.sh
-
-if [[ -z "$REPLICATE_FROM" ]]; then
-	# This means this is a master instance. We check that database exists
-	echo "Setup master database"
-	source /setup-database.sh
-else
-	# This means this is a slave/replication instance.
-	echo "Setup slave database"
-	source /setup-replication.sh
-fi
-
-function kill_postgres {
-PID=`cat ${PG_PID}`
-kill -TERM ${PID}
-
-# Wait for background postgres main process to exit
-while [[ "$(ls -A ${PG_PID} 2>/dev/null)" ]]; do
-  sleep 1
-done
-}
-
 
 # Running extended script or sql if provided.
 # Useful for people who extends the image.
@@ -65,8 +43,35 @@ else
 fi
 
 }
-entry_point_script
-kill_postgres
+
+function kill_postgres {
+PID=`cat ${PG_PID}`
+kill -TERM ${PID}
+
+# Wait for background postgres main process to exit
+while [[ "$(ls -A ${PG_PID} 2>/dev/null)" ]]; do
+  sleep 1
+done
+}
+
+if [[ -z "$REPLICATE_FROM" ]]; then
+	# This means this is a master instance. We check that database exists
+	echo "Setup master database"
+	source /setup-database.sh
+	entry_point_script
+	kill_postgres
+else
+	# This means this is a slave/replication instance.
+	echo "Setup slave database"
+	source /setup-replication.sh
+fi
+
+
+
+
+
+
+
 # If no arguments passed to entrypoint, then run postgres by default
 if [[ $# -eq 0 ]];
 then
