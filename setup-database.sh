3ac7f657a3a10e972d897921dc7d6b370f806042
admire <admire@kartoza.com>
2019-08-23 12:49:27 +0200

    Added env variables for postgresql.conf and new settings available in PG 11

--- setup-database.sh
+++ setup-database.sh
@@ -31,7 +31,7 @@ trap "echo \"Sending SIGTERM to postgres\"; killall -s SIGTERM postgres" SIGTERM
 
 
 
-su - postgres -c "${POSTGRES} -D ${DATADIR} -c config_file=${CONF} ${LOCALONLY} &"
+su - postgres -c "${POSTGRES} -D ${DATADIR} --wal-segsize=size 1024 -c config_file=${CONF} ${LOCALONLY} &"
 
 # wait for postgres to come up
 until su - postgres -c "psql -l"; do
