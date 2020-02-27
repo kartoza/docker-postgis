732e25be964996370ccc1cfc22f996ec6f61f02a
Denis <fanaticFiz@outlook.com>
2020-02-27 11:07:56 +0300

    Fix issue 210. chown to user postgres must be before init db

--- setup-database.sh
+++ setup-database.sh
@@ -9,25 +9,30 @@ SETUP_LOCKFILE="${DATADIR}/.postgresql.init.lock"
 
 # test if DATADIR has content
 if [[ -z "${EXISTING_DATA_DIR}" ]]; then \
+
+    # test if DATADIR is existent
+    if [[ ! -d ${DATADIR} ]]; then
+      echo "Creating Postgres data at ${DATADIR}"
+      mkdir -p ${DATADIR}
+    fi
+
+    # Set proper permissions needs to be done as root:
+    chown -R postgres:postgres ${DATADIR}
+
     if [[ ! -f "${SETUP_LOCKFILE}" ]]; then
         # No content yet - first time pg is being run!
         # No Replicate From settings. Assume that this is a master database.
         # Initialise db
         echo "Initializing Postgres Database at ${DATADIR}"
-        rm -rf ${DATADIR}
+        # rm -rf ${DATADIR}
         #chown -R postgres $DATADIR
         su - postgres -c "$INITDB -E ${DEFAULT_ENCODING} --lc-collate=${DEFAULT_COLLATION} --lc-ctype=${DEFAULT_CTYPE} --wal-segsize=${WAL_SEGSIZE} ${DATADIR}"
         touch ${SETUP_LOCKFILE}
     fi
-
 fi;
-# Set proper permissions
-# needs to be done as root:
-chown -R postgres:postgres ${DATADIR}
 
 # test database existing
 trap "echo \"Sending SIGTERM to postgres\"; killall -s SIGTERM postgres" SIGTERM
-
 
 
 su - postgres -c "${POSTGRES} -D ${DATADIR} -c config_file=${CONF} ${LOCALONLY} &"
