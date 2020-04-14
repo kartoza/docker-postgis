ba4b9672f60f54313dd176d8e6fe91fd44a26ff4
admire <admire@kartoza.com>
2020-04-14 11:42:30 +0200

    Increase timeout for collation tests

--- scenario_tests/collations/test.sh
+++ scenario_tests/collations/test.sh
@@ -8,7 +8,7 @@ source ../test-env.sh
 # Run service
 docker-compose up -d
 
-sleep 5
+sleep 30
 
 services=("pg" "pg-new")
 
@@ -16,7 +16,7 @@ for service in "${services[@]}"; do
 
   # Execute tests
   until docker-compose exec $service pg_isready; do
-    sleep 1
+    sleep 30
   done;
   docker-compose exec $service /bin/bash /tests/test.sh
 
