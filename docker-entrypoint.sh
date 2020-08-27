764d889b049ba6b5be55f576da5d8aeeb165c0bc
Adam Hodges <ahodges@shipchain.io>
2020-08-27 13:43:27 -0400

    Cherrypicking PR kartoza#127 into 10.0-2.4

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,29 +12,63 @@ source /setup-ssl.sh
 # Setup pg_hba.conf
 source /setup-pg_hba.sh
 
-if [ -z "$REPLICATE_FROM" ]; then
+# Running extended script or sql if provided.
+# Useful for people who extends the image.
+function entry_point_script {
+SETUP_LOCKFILE="/docker-entrypoint-initdb.d/.entry_point.lock"
+if [[ -f "${SETUP_LOCKFILE}" ]]; then
+	return 0
+else
+    if find "/docker-entrypoint-initdb.d" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
+        for f in /docker-entrypoint-initdb.d/*; do
+        export PGPASSWORD=${POSTGRES_PASS}
+        list=(`echo ${POSTGRES_DBNAME} | tr ',' ' '`)
+        arr=(${list})
+        SINGLE_DB=${arr[0]}
+        case "$f" in
+            *.sql)    echo "$0: running $f"; psql ${SINGLE_DB} -U ${POSTGRES_USER} -p 5432 -h localhost  -f ${f} || true ;;
+            *.sql.gz) echo "$0: running $f"; gunzip < "$f" | psql ${SINGLE_DB} -U ${POSTGRES_USER} -p 5432 -h localhost || true ;;
+            *)        echo "$0: ignoring $f" ;;
+        esac
+        echo
+        done
+        # Put lock file to make sure entry point scripts were run
+        touch ${SETUP_LOCKFILE}
+    else
+        return 0
+    fi
+
+fi
+
+}
+
+function kill_postgres {
+PID=`cat ${PG_PID}`
+kill -TERM ${PID}
+
+# Wait for background postgres main process to exit
+while [[ "$(ls -A ${PG_PID} 2>/dev/null)" ]]; do
+  sleep 1
+done
+}
+
+if [[ -z "$REPLICATE_FROM" ]]; then
 	# This means this is a master instance. We check that database exists
 	echo "Setup master database"
 	source /setup-database.sh
+	entry_point_script
+	kill_postgres
 else
 	# This means this is a slave/replication instance.
 	echo "Setup slave database"
 	source /setup-replication.sh
 fi
 
-# Running extended script or sql if provided.
-# Useful for people who extends the image.
 
-echo
-for f in /docker-entrypoint-initdb.d/*; do
-	case "$f" in
-		*.sh)     echo "$0: running $f"; . "$f" ;;
-		*.sql)    echo "$0: running $f"; "${psql[@]}" < "$f"; echo ;;
-		*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${psql[@]}"; echo ;;
-		*)        echo "$0: ignoring $f" ;;
-	esac
-	echo
-done
+
+
+
+
 
 # If no arguments passed to entrypoint, then run postgres by default
 if [ $# -eq 0 ];
