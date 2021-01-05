ea053db2dd62dc385e0595d1dc83b4fcbe046d54
admire <admire@kartoza.com>
2021-01-05 11:39:02 +0200

    Fix build script to reflect building correct version

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cd base_build
 ./build.sh
 cd ..
 docker build -t kartoza/postgis:manual-build .
-docker build --build-arg DISTRO=debian --build-arg IMAGE_VERSION=bullseye --build-arg IMAGE_VARIANT=slim -t kartoza/postgis:${POSTGRES_MAJOR_VERSION}.0 .
+docker build --build-arg DISTRO=debian --build-arg IMAGE_VERSION=bullseye --build-arg IMAGE_VARIANT=slim -t kartoza/postgis:${POSTGRES_MAJOR_VERSION}.1 .
