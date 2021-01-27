dd0613f56e56d968133b067560bab2e78cdd498d
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2021-01-27 17:36:24 +0700

    Implement Github Action for base image

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 POSTGRES_MAJOR_VERSION=13
 POSTGIS_MINOR_RELEASE=1
 
-cd base_build
+pushd base_build
 ./build.sh
-cd ..
+popd
 docker build -t kartoza/postgis:manual-build .
 docker build --build-arg DISTRO=debian --build-arg IMAGE_VERSION=bullseye --build-arg IMAGE_VARIANT=slim -t kartoza/postgis:${POSTGRES_MAJOR_VERSION}.${POSTGIS_MINOR_RELEASE} .
