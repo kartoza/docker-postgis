5f125af29c6475a2f66330f394b039f9f2d77813
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2021-01-28 00:04:40 +0000

    Fix travis

--- build-base.sh
+++ build-base.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # Building an debian base image
 
+if [[ ! -f .env ]]; then
+    echo "Default build arguments don't exists. Creating one from default value."
+    cp .example.env .env
+fi
+
 docker-compose -f docker-compose.build.yml build postgis-base
 
--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [[ ! -f .env ]]; then
+    echo "Default build arguments don't exists. Creating one from default value."
+    cp .example.env .env
+fi
+
 docker-compose -f docker-compose.build.yml build postgis-prod
