d4ad04e57fc8c50ff9d73cbe6f27f19d704a11c6
mazano <addloe@gmail.com>
2022-12-02 13:44:07 +0200

    fix pg_hba (#396)
    * fix unreported bug with mismatch values in pg_hba.conf and fix health check test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,12 @@ source /scripts/env-data.sh
 
 # Create backup template for conf
 cat $CONF > $CONF.template
+
+# Create backup template for pg_hba.conf
+sed -i 's/scram-sha-256/${PASSWORD_AUTHENTICATION}/g' $ROOT_CONF/pg_hba.conf
+sed -i 's/md5/${PASSWORD_AUTHENTICATION}/g' $ROOT_CONF/pg_hba.conf
+
+
 cat $ROOT_CONF/pg_hba.conf > $ROOT_CONF/pg_hba.conf.template
+
+
