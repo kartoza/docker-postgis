e7fd886c38fc1d0031095b19c312b57f720cd2f8
mazano <addloe@gmail.com>
2022-12-20 17:43:58 +0200

    minor tweaks (#401)

--- build.sh
+++ build.sh
@@ -4,15 +4,19 @@ if [[ ! -f .env ]]; then
     echo "Default build arguments don't exists. Creating one from default value."
     cp .example.env .env
 fi
+source .env
 
-if [[ $(dpkg -l | grep "docker-compose") > /dev/null ]];then
-  docker-compose -f docker-compose.build.yml build postgis-prod
+if [[ ${BUILD_TIMESCALE} =~ [Ff][Aa][Ll][Ss][Ee] ]];then
+  if [[ $(dpkg -l | grep "docker-compose") > /dev/null ]];then
+    docker-compose -f docker-compose.build.yml build postgis-prod
+  else
+    docker compose -f docker-compose.build.yml build postgis-prod
+  fi
+
 else
-  docker compose -f docker-compose.build.yml build postgis-prod
+  if [[ $(dpkg -l | grep "docker-compose") > /dev/null ]];then
+  docker-compose -f docker-compose.build-timescale.yml build postgis-prod
+  else
+  docker compose -f docker-compose.build-timescale.yml build postgis-prod
+  fi
 fi
-
-#if [[ $(dpkg -l | grep "docker-compose") > /dev/null ]];then
-  #docker-compose -f docker-compose.build-timescale.yml build postgis-prod
-#else
-  #docker compose -f docker-compose.build-timescale.yml build postgis-prod
-#fi
