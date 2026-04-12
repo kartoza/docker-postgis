869313da8dab96c18a8dfed59b06cb4e7ec2082c
Admire Nyakudya <mazano@MacBookAir.Dlink>
2026-04-12 20:34:26 +0200

    fix replication

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,9 +5,8 @@ set -e
 #############################################
 # Bootstrap
 #############################################
-# Import env and functions
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "${SCRIPT_DIR}/lib/env-data.sh"
 source "${SCRIPT_DIR}/lib/utils.sh"
@@ -15,7 +14,9 @@ source "${SCRIPT_DIR}/lib/postgres.sh"
 source "${SCRIPT_DIR}/lib/setup-conf.sh"
 source "${SCRIPT_DIR}/lib/setup-ssl.sh"
 source "${SCRIPT_DIR}/lib/setup-pg_hba.sh"
-
+#############################################
+# Setup
+#############################################
 # Reset readiness marker on container start
 start_postgres_marker
 
@@ -33,33 +34,20 @@ entrypoint_figlet
 # Gosu preparations
 if [[ ${RUN_AS_ROOT} =~ [Ff][Aa][Ll][Ss][Ee] ]];then
   setup_postgres_users
-
-  if [[ "${REPLICATION}" =~ [Tt][Rr][Uu][Ee] ]] ; then
-    echo "/home/${USER_NAME}/.pgpass" > /tmp/pg_subs.txt
-    envsubst < /tmp/pg_subs.txt > /tmp/pass_command.txt
-    PGPASSFILE=$(cat /tmp/pass_command.txt)
-    rm /tmp/pg_subs.txt /tmp/pass_command.txt
-  fi
-
 fi
 
-if [[ -f /scripts/.pass_20.txt ]]; then
-  USER_CREDENTIAL_PASS=$(cat /scripts/.pass_20.txt)
-  cp /scripts/.pass_20.txt /tmp/PGPASSWORD.txt
-  echo -e "[Entrypoint] GENERATED Postgres  PASSWORD: \e[1;31m $USER_CREDENTIAL_PASS \033[0m"
-fi
+expose_replication
 
-if [[ -f /scripts/.pass_22.txt ]]; then
-  USER_CREDENTIAL_PASS=$(cat /scripts/.pass_22.txt)
-  cp /scripts/.pass_22.txt /tmp/REPLPASSWORD.txt
-  echo -e "[Entrypoint] GENERATED Replication  PASSWORD: \e[1;34m $USER_CREDENTIAL_PASS \033[0m"
-fi
-
+expose_credentials
 
 run_streaming_replication
 
-# If no arguments passed to entrypoint, then run postgres by default
 
+#############################################
+# Entrypoint
+#############################################
+
+# If no arguments passed to entrypoint, then run postgres by default
 if [[ $# -eq 0 ]]; then
   echo -e "[Entrypoint] Postgres initialisation process completed .... starting final postgres"
 
