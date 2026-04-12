a19f8173266459576293830539bc42dac25a7386
Admire Nyakudya <mazano@za-nyakudya-mac.local>
2026-04-12 08:16:47 +0200

    continue refactor

--- scripts/lib/env-data.sh
+++ scripts/lib/env-data.sh
@@ -1,38 +1,8 @@
 #!/usr/bin/env bash
 
-
-
 #############################################
-# Env variables
+# Functions
 #############################################
-
-POSTGRES_MAJOR_VERSION=$(cat /tmp/pg_version.txt)
-POSTGIS_MAJOR=$(cat /tmp/pg_major_version.txt)
-POSTGIS_MINOR_RELEASE=$(cat /tmp/pg_minor_version.txt)
-DEFAULT_DATADIR="/var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main"
-# Commented for documentation. You can specify the location of
-# pg_wal directory/volume using the following environment variable:
-# POSTGRES_INITDB_WALDIR (default value is unset)
-DEFAULT_SCRIPTS_LOCKFILE_DIR="/docker-entrypoint-initdb.d"
-DEFAULT_CONF_LOCKFILE_DIR="/settings"
-DEFAULT_EXTRA_CONF_DIR="/settings"
-ROOT_CONF="/etc/postgresql/${POSTGRES_MAJOR_VERSION}/main"
-PG_ENV="$ROOT_CONF/environment"
-CONF="$ROOT_CONF/postgresql.conf"
-DEFAULT_WAL_ARCHIVE="/opt/archivedir"
-RECOVERY_CONF="$ROOT_CONF/recovery.conf"
-POSTGRES="/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/postgres"
-INITDB="/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/initdb"
-SQLDIR="/usr/share/postgresql/${POSTGRES_MAJOR_VERSION}/contrib/postgis-${POSTGIS_MAJOR}.${POSTGIS_MINOR_RELEASE}/"
-EXTDIR="/usr/share/postgresql/${POSTGRES_MAJOR_VERSION}/extension/"
-SETVARS="POSTGIS_ENABLE_OUTDB_RASTERS=1 POSTGIS_GDAL_ENABLED_DRIVERS=ENABLE_ALL"
-LOCALONLY="-c listen_addresses='127.0.0.1'"
-PG_BASEBACKUP="/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_basebackup"
-NODE_PROMOTION="/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_ctl"
-DATA_DIR_CONTROL="/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_controldata"
-PGSTAT_TMP="/var/run/postgresql/"
-PG_PID="/var/run/postgresql/${POSTGRES_MAJOR_VERSION}-main.pid"
-
 file_env() {
 	local var="$1"
 	local fileVar="${var}_FILE"
@@ -71,28 +41,62 @@ boolean() {
   esac
 }
 
+env_default() {
+  local var="$1"
+  local default="$2"
 
+  if [[ -z "${!var:-}" ]]; then
+    export "${var}=${default}"
+  fi
+}
+
+#############################################
+# Env variables
+#############################################
+
+env_default POSTGRES_MAJOR_VERSION "$(cat /tmp/pg_version.txt)"
+env_default POSTGIS_MAJOR "$(cat /tmp/pg_major_version.txt)"
+env_default POSTGIS_MINOR_RELEASE "$(cat /tmp/pg_minor_version.txt)"
+env_default DEFAULT_DATADIR "/var/lib/postgresql/${POSTGRES_MAJOR_VERSION}/main"
+env_default DEFAULT_SCRIPTS_LOCKFILE_DIR "/docker-entrypoint-initdb.d"
+env_default DEFAULT_CONF_LOCKFILE_DIR "/settings"
+env_default DEFAULT_EXTRA_CONF_DIR "/settings"
+env_default ROOT_CONF "/etc/postgresql/${POSTGRES_MAJOR_VERSION}/main"
+env_default PG_ENV "$ROOT_CONF/environment"
+env_default CONF "$ROOT_CONF/postgresql.conf"
+env_default DEFAULT_WAL_ARCHIVE "/opt/archivedir"
+env_default RECOVERY_CONF "$ROOT_CONF/recovery.conf"
+env_default POSTGRES "/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/postgres"
+env_default INITDB "/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/initdb"
+env_default SQLDIR "/usr/share/postgresql/${POSTGRES_MAJOR_VERSION}/contrib/postgis-${POSTGIS_MAJOR}.${POSTGIS_MINOR_RELEASE}/"
+env_default EXTDIR "/usr/share/postgresql/${POSTGRES_MAJOR_VERSION}/extension/"
+env_default SETVARS "POSTGIS_ENABLE_OUTDB_RASTERS=1 POSTGIS_GDAL_ENABLED_DRIVERS=ENABLE_ALL"
+env_default LOCALONLY "-c listen_addresses='127.0.0.1'"
+env_default PG_BASEBACKUP "/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_basebackup"
+env_default NODE_PROMOTION "/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_ctl"
+env_default DATA_DIR_CONTROL "/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_controldata"
+env_default PGSTAT_TMP "/var/run/postgresql/"
+env_default PG_PID "/var/run/postgresql/${POSTGRES_MAJOR_VERSION}-main.pid"
+
+########################################
+# Core defaults
+########################################
 file_env 'POSTGRES_PASS'
 file_env 'POSTGRES_USER'
 
-# Make sure we have a user set up
-if [ -z "${POSTGRES_USER}" ]; then
-	POSTGRES_USER=docker
-fi
 
+env_default POSTGRES_USER "docker"
+env_default POSTGRES_DBNAME "gis"
+env_default DATADIR "${DEFAULT_DATADIR}"
+env_default SSL_DIR "/ssl_certificates"
+env_default WAL_ARCHIVE "${DEFAULT_WAL_ARCHIVE}"
+env_default SCRIPTS_LOCKFILE_DIR "${DEFAULT_SCRIPTS_LOCKFILE_DIR}"
+env_default CONF_LOCKFILE_DIR "${DEFAULT_CONF_LOCKFILE_DIR}"
+env_default EXTRA_CONF_DIR "${DEFAULT_EXTRA_CONF_DIR}"
 
 if [ -z "${POSTGRES_PASS}" ]; then
   generate_random_string 20
 	POSTGRES_PASS=${RAND}
-fi
-
-
-if [ -z "${POSTGRES_DBNAME}" ]; then
-	POSTGRES_DBNAME=gis
-fi
-# If datadir is not defined, then use this
-if [ -z "${DATADIR}" ]; then
-  DATADIR=${DEFAULT_DATADIR}
 fi
 
 # RECREATE_DATADIR flag default value
@@ -103,82 +107,26 @@ if [ -z "${RECREATE_DATADIR}" ]; then
 else
   RECREATE_DATADIR=$(boolean ${RECREATE_DATADIR})
 fi
-
-if [ -z "${SSL_DIR}" ]; then
-  SSL_DIR="/ssl_certificates"
-fi
-
-if [ -z "${WAL_ARCHIVE}" ]; then
-   WAL_ARCHIVE=${DEFAULT_WAL_ARCHIVE}
-fi
-
-if [ -z "${SCRIPTS_LOCKFILE_DIR}" ]; then
-   SCRIPTS_LOCKFILE_DIR=${DEFAULT_SCRIPTS_LOCKFILE_DIR}
-fi
-
-if [ -z "${CONF_LOCKFILE_DIR}" ]; then
-   CONF_LOCKFILE_DIR=${DEFAULT_CONF_LOCKFILE_DIR}
-fi
-
-if [ -z "${EXTRA_CONF_DIR}" ]; then
-   EXTRA_CONF_DIR=${DEFAULT_EXTRA_CONF_DIR}
-fi
-
 # Enable hstore and topology by default
-if [ -z "${HSTORE}" ]; then
-	HSTORE=true
-fi
-if [ -z "${TOPOLOGY}" ]; then
-	TOPOLOGY=true
-fi
+env_default HSTORE "true"
+env_default TOPOLOGY "true"
 # Replication settings
-
-if [ -z "${REPLICATION}" ]; then
-	REPLICATION=false
-fi
-if [ -z "${REPLICATE_PORT}" ]; then
-	REPLICATE_PORT=5432
-fi
-if [ -z "${DESTROY_DATABASE_ON_RESTART}" ]; then
-	DESTROY_DATABASE_ON_RESTART=true
-fi
-if [ -z "${PG_MAX_WAL_SENDERS}" ]; then
-	PG_MAX_WAL_SENDERS=10
-fi
-if [ -z "${PG_WAL_KEEP_SIZE}" ]; then
-	PG_WAL_KEEP_SIZE=20
-fi
-
+env_default REPLICATION "false"
+env_default REPLICATE_PORT "5432"
+env_default DESTROY_DATABASE_ON_RESTART "true"
+env_default PG_MAX_WAL_SENDERS "10"
+env_default PG_WAL_KEEP_SIZE "20"
 
 #Logical replication settings
-if [ -z "${MAX_LOGICAL_REPLICATION_WORKERS}" ]; then
-  MAX_LOGICAL_REPLICATION_WORKERS=4
-fi
+env_default MAX_LOGICAL_REPLICATION_WORKERS "4"
+env_default MAX_SYNC_WORKERS_PER_SUBSCRIPTION "2"
+env_default IP_LIST "*"
+env_default MAINTENANCE_WORKERS "2"
+env_default ARCHIVE_MODE "off"
+env_default ARCHIVE_COMPRESSION "gzip"
+env_default ARCHIVE_DECOMPRESSION "gunzip"
 
-if [ -z "${MAX_SYNC_WORKERS_PER_SUBSCRIPTION}" ]; then
-  MAX_SYNC_WORKERS_PER_SUBSCRIPTION=2
-fi
 
-if [ -z "${IP_LIST}" ]; then
-	IP_LIST='*'
-fi
-
-if [ -z "${MAINTENANCE_WORKERS}" ]; then
-	MAINTENANCE_WORKERS=2
-fi
-
-if [ -z "${ARCHIVE_MODE}" ]; then
-  # https://www.postgresql.org/docs/12/runtime-config-wal.html
-  ARCHIVE_MODE=off
-fi
-
-if [ -z "${ARCHIVE_COMPRESSION}" ]; then
-  ARCHIVE_COMPRESSION=gzip
-fi
-
-if [ -z "${ARCHIVE_DECOMPRESSION}" ]; then
-  ARCHIVE_DECOMPRESSION=gunzip
-fi
 
 if [ -z "${ARCHIVE_COMMAND}" ]; then
   # https://www.postgresql.org/docs/14/continuous-archiving.html#BACKUP-ARCHIVING-
@@ -198,124 +146,36 @@ if [ -z "${RESTORE_COMMAND}" ]; then
   fi
 fi
 
-if [ -z "${ARCHIVE_CLEANUP_COMMAND}" ]; then
-  # https://www.postgresql.org/docs/12/runtime-config-wal.html
-  ARCHIVE_CLEANUP_COMMAND="/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_archivecleanup ${WAL_ARCHIVE} %r"
-fi
-
-if [ -z "${WAL_LEVEL}" ]; then
-  # https://www.postgresql.org/docs/12/runtime-config-wal.html
-	WAL_LEVEL=replica
-fi
-
-if [ -z "${WAL_SIZE}" ]; then
-	WAL_SIZE=4GB
-fi
-
-if [ -z "${MIN_WAL_SIZE}" ]; then
-	MIN_WAL_SIZE=1024MB
-fi
-
-if [ -z "${WAL_SEGSIZE}" ]; then
-	WAL_SEGSIZE=32
-fi
-
-if [ -z "${SHARED_BUFFERS}" ]; then
-	SHARED_BUFFERS=256MB
-fi
-
-if [ -z "${WORK_MEM}" ]; then
-	WORK_MEM=16MB
-fi
-
-if [ -z "${WAL_BUFFERS}" ]; then
-	WAL_BUFFERS=1MB
-fi
-
-if [ -z "${CHECK_POINT_TIMEOUT}" ]; then
-	CHECK_POINT_TIMEOUT=30min
-fi
-
-if [ -z "${MAX_WORKERS}" ]; then
-	MAX_WORKERS=4
-fi
-
-if [ -z "${MAINTENANCE_WORK_MEM}" ]; then
-	MAINTENANCE_WORK_MEM=128MB
-fi
-
-
-if [ -z "${SSL_CERT_FILE}" ]; then
-	SSL_CERT_FILE='/etc/ssl/certs/ssl-cert-snakeoil.pem'
-fi
-
-if [ -z "${SSL_KEY_FILE}" ]; then
-	SSL_KEY_FILE='/etc/ssl/private/ssl-cert-snakeoil.key'
-fi
+env_default ARCHIVE_CLEANUP_COMMAND "/usr/lib/postgresql/${POSTGRES_MAJOR_VERSION}/bin/pg_archivecleanup ${WAL_ARCHIVE} %r"
+env_default WAL_LEVEL "replica"
+env_default WAL_SIZE "4GB"
+env_default MIN_WAL_SIZE "1024MB"
+env_default WAL_SEGSIZE "32"
+env_default SHARED_BUFFERS "256MB"
+env_default WORK_MEM "16MB"
+env_default WAL_BUFFERS "1MB"
+env_default CHECK_POINT_TIMEOUT "30min"
+env_default MAX_WORKERS "4"
+env_default MAINTENANCE_WORK_MEM "128MB"
+env_default SSL_CERT_FILE "/etc/ssl/certs/ssl-cert-snakeoil.pem"
+env_default SSL_KEY_FILE "/etc/ssl/private/ssl-cert-snakeoil.key"
 
 # controls all login params below, defaults to false
-if [ -z "${LOGGING}" ]; then
-  LOGGING='FALSE'
-fi
-# log
-
-if [ -z "${LOGGING_COLLECTOR}" ]; then
-  LOGGING_COLLECTOR='off'
-fi
-
-if [ -z "${LOG_DIRECTORY}" ]; then
-  LOG_DIRECTORY='pg_log'
-fi
-
-if [ -z "${LOG_FILENAME}" ]; then
-  LOG_FILENAME='postgresql-%Y-%m-%d_%H%M%S.log'
-fi
-
-if [ -z "${LOG_ROTATION_AGE}" ]; then
-  LOG_ROTATION_AGE='1d'
-fi
-
-if [ -z "${LOG_ROTATION_SIZE}" ]; then
-  LOG_ROTATION_SIZE='100MB'
-fi
-
-if [ -z "${LOG_TRUNCATE_ON_ROTATION}" ]; then
-  LOG_TRUNCATE_ON_ROTATION='on'
-fi
-
-if [ -z "${LOG_LOCK_WAITS}" ]; then
-  LOG_LOCK_WAITS='on'
-fi
-
-if [ -z "${LOG_DURATION}" ]; then
-  LOG_DURATION='on'
-fi
-
-if [ -z "${LOG_STATEMENT}" ]; then
-  LOG_STATEMENT='all'
-fi
-
-if [ -z "${LOG_MIN_DURATION_STATEMENT}" ]; then
-  LOG_MIN_DURATION_STATEMENT='20'
-fi
-
-if [ -z "${LOG_CONNECTIONS}" ]; then
-  LOG_CONNECTIONS='on'
-fi
-
-if [ -z "${LOG_DISCONNECTS}" ]; then
-  LOG_DISCONNECTS='on'
-fi
-
-if [ -z "${LOG_LINE_PREFIX}" ]; then
-  LOG_LINE_PREFIX='%m [%p]: [%l-1] %u@%d '
-fi
-
-if [ -z "${LOG_TIMEZONE}" ]; then
-  LOG_TIMEZONE='Etc/UTC'
-fi
-
-
+env_default LOGGING "FALSE"
+env_default LOGGING_COLLECTOR "off"
+env_default LOG_DIRECTORY "pg_log"
+env_default LOG_FILENAME "postgresql-%Y-%m-%d_%H%M%S.log"
+env_default LOG_ROTATION_AGE "1d"
+env_default LOG_ROTATION_SIZE "100MB"
+env_default LOG_TRUNCATE_ON_ROTATION "on"
+env_default LOG_LOCK_WAITS "on"
+env_default LOG_DURATION "on"
+env_default LOG_STATEMENT "all"
+env_default LOG_MIN_DURATION_STATEMENT "20"
+env_default LOG_CONNECTIONS "on"
+env_default LOG_DISCONNECTS "on"
+env_default LOG_LINE_PREFIX "%m [%p]: [%l-1] %u@%d "
+env_default LOG_TIMEZONE "Etc/UTC"
 # SSL mode
 
 if [ -z "${POSTGRES_MULTIPLE_EXTENSIONS}" ]; then
@@ -326,54 +186,23 @@ if [ -z "${POSTGRES_MULTIPLE_EXTENSIONS}" ]; then
     fi
 fi
 
-
-if [ -z "${ALLOW_IP_RANGE}" ]; then
-  ALLOW_IP_RANGE='0.0.0.0/0'
-fi
-
-if [ -z "${DEFAULT_ENCODING}" ]; then
-  DEFAULT_ENCODING="UTF8"
-fi
-
-if [ -z "${PGCLIENTENCODING}" ]; then
-  PGCLIENTENCODING="UTF8"
-fi
-
-if [ -z "${DEFAULT_COLLATION}" ]; then
-  DEFAULT_COLLATION="en_US.UTF-8"
-fi
-if [ -z "${DEFAULT_CTYPE}" ]; then
-  DEFAULT_CTYPE="en_US.UTF-8"
-fi
-
-if [ -z "${TARGET_TIMELINE}" ]; then
-	TARGET_TIMELINE='latest'
-fi
-
-if [ -z "${TARGET_ACTION}" ]; then
-	TARGET_ACTION='promote'
-fi
-
-if [ -z "${REPLICATION_USER}" ]; then
-  REPLICATION_USER=replicator
-fi
+env_default ALLOW_IP_RANGE "0.0.0.0/0"
+env_default DEFAULT_ENCODING "UTF8"
+env_default PGCLIENTENCODING "UTF8"
+env_default DEFAULT_COLLATION "en_US.UTF-8"
+env_default DEFAULT_CTYPE "en_US.UTF-8"
+env_default TARGET_TIMELINE "latest"
+env_default TARGET_ACTION "promote"
+env_default REPLICATION_USER "replicator"
 
 if [ -z "${REPLICATION_PASS}" ]; then
   generate_random_string 22
 	REPLICATION_PASS=${RAND}
 fi
 
-if [ -z "$IGNORE_INIT_HOOK_LOCKFILE" ]; then
-    IGNORE_INIT_HOOK_LOCKFILE=false
-fi
-
-if [ -z "$EXTRA_CONF" ]; then
-    EXTRA_CONF=""
-fi
-
-if [ -z "$ACTIVATE_CRON" ]; then
-    ACTIVATE_CRON=TRUE
-fi
+env_default IGNORE_INIT_HOOK_LOCKFILE "false"
+env_default EXTRA_CONF ""
+env_default ACTIVATE_CRON "TRUE"
 
 if [ -z "${SHARED_PRELOAD_LIBRARIES}" ]; then
     if [[ $(dpkg -l | grep "timescaledb") > /dev/null ]];then
@@ -389,33 +218,20 @@ if [ -z "${SHARED_PRELOAD_LIBRARIES}" ]; then
     fi
 fi
 
-if [ -z "$PASSWORD_AUTHENTICATION" ]; then
-    PASSWORD_AUTHENTICATION="scram-sha-256"
-fi
 
-if [ -z "${ALL_DATABASES}" ]; then
-  ALL_DATABASES=FALSE
-fi
 
-if [ -z "${FORCE_SSL}" ]; then
-  FORCE_SSL=FALSE
-fi
+env_default PASSWORD_AUTHENTICATION "scram-sha-256"
+env_default ALL_DATABASES "FALSE"
+env_default FORCE_SSL "FALSE"
+env_default ACCEPT_TIMESCALE_TUNING "FALSE"
+env_default TIMESCALE_TUNING_PARAMS ""
+env_default TIMESCALE_TUNING_CONFIG "time_scale_tuning.conf"
+env_default RUN_AS_ROOT "true"
+env_default TIMEZONE "Etc/UTC"
+env_default KERNEL_SHMMAX "543252480"
+env_default KERNEL_SHMALL "2097152"
+env_default PROMOTE_MASTER "FALSE"
 
-if [ -z "${ACCEPT_TIMESCALE_TUNING}" ]; then
-  ACCEPT_TIMESCALE_TUNING=FALSE
-fi
-
-if [ -z "${TIMESCALE_TUNING_PARAMS}" ]; then
-  TIMESCALE_TUNING_PARAMS=
-fi
-
-if [ -z "${TIMESCALE_TUNING_CONFIG}" ]; then
-  TIMESCALE_TUNING_CONFIG=time_scale_tuning.conf
-fi
-
-if [ -z "${RUN_AS_ROOT}" ]; then
-  RUN_AS_ROOT=true
-fi
 
 # Compatibility with official postgres variable
 # Official postgres variable gets priority
@@ -439,20 +255,4 @@ read -a dbarr <<< "$POSTGRES_DBNAME"
 export SINGLE_DB=${dbarr[0]}
 
 
-
-if [ -z "${TIMEZONE}" ]; then
-  TIMEZONE='Etc/UTC'
-fi
-
-if [ -z "${KERNEL_SHMMAX}" ]; then
-  KERNEL_SHMMAX=543252480
-fi
-
-if [ -z "${KERNEL_SHMALL}" ]; then
-  KERNEL_SHMALL=2097152
-fi
-
-if [ -z "${PROMOTE_MASTER}" ]; then
-  PROMOTE_MASTER=FALSE
-fi
 
