013dad4e469dda8c32def48432f009f5e8799623
Admire Nyakudya <mazano@za-nyakudya-mac.local>
2026-05-16 19:44:00 +0200

    add submodule

--- scripts/compile_extensions.sh
+++ scripts/compile_extensions.sh
@@ -19,6 +19,7 @@ apt install -y  \
 
 git clone https://github.com/duckdb/pg_duckdb
 cd pg_duckdb
+git submodule update --init --recursive
 make -j 4 install
 cd ..
 apt purge -y git;rm -rf pg_duckdb
