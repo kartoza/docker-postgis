0070566711d00d58dfaec91121c8f2da560b6830
Admire Nyakudya <mazano@MacBookAir.Dlink>
2026-06-16 19:07:10 +0200

    fix mount path

--- .github/workflows/scripts/get_postgis_version.sh
+++ .github/workflows/scripts/get_postgis_version.sh
@@ -62,9 +62,9 @@ printf '%s\n' \
     "PG_VER_PROD=${PG_VER_PROD}" \
     "GIS_VER_PROD=${GIS_VER_PROD}" \
     "BASE_IMAGE_DIGEST_SHA=${BASE_IMAGE_SHA}" \
-    > /tmp/github_output.txt
+    > /github_output/github_output.txt
 
-cat /tmp/github_output.txt
+cat /github_output/github_output.txt
 echo "Script finished OK"
 
 
